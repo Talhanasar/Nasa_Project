7349bc182a4b5692638831f53483bb88869a3502
Talhanasar <talhabinnasar7@gmail.com>
2024-09-13 21:49:31 +0600

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage'
 import LandingPage from './pages/LandingPage'
 import AboutPage from './pages/AboutPage'
-import AppLayout from './AppLayout'
 
 function App() {
 
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<AppLayout/>,
-      errorElement:<ErrorPage/>,
-      children:([
-        {
-          path: "/",
-          element: <LandingPage />,
-        },
-        {
-          path: "/about",
-          element: <AboutPage />,
-        }
-
-      ])
+      path: "/",
+      element: <LandingPage />,
     },
+    {
+      path: "/about",
+      element: <AboutPage />,
+    },
+    {
+      path: "*", 
+      element: <ErrorPage />, 
+    }
   ])
 
   return (
