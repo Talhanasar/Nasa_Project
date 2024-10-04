c2a907d38b22f2b7775b7365b94af62423df37d3
Talhanasar <talhabinnasar7@gmail.com>
2024-10-04 23:56:04 +0600

    Update Geomagnetic.jsx

--- src/pages/Geomagnetic.jsx
+++ src/pages/Geomagnetic.jsx
@@ -4,7 +4,6 @@ import '../css/Geomagnetic.css'
 const Geomagnetic = () => {
   return (
     <div className='main-geomagnetic'>
-     <video src="/video/geomagnetic storm.mp4" autoPlay loop muted></video>
     </div>
   )
 }
